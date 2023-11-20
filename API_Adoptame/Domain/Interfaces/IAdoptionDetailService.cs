cac5194d9a1d6242418f3cad6cd5365474eafd17
samalxMia <126675804+samalxMia@users.noreply.github.com>
2023-11-19 22:08:03 -0500

    update interfaces

---
+++ API_Adoptame/Domain/Interfaces/IAdoptionDetailService.cs
@@ -0,0 +1,10 @@
+﻿using API_Adoptame.DAL.Entities;
+
+namespace API_Adoptame.Domain.Interfaces
+{
+    public interface IAdoptionDetailService
+    {
+        Task<IEnumerable<AdoptionDetail>> GetAdoptionDetailAsync();
+        Task<AdoptionDetail> CreateAdoptionDetailAsync(AdoptionDetail adoptionDetail);
+    }
+}
