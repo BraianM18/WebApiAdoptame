4c233b2d4310e79d8a0207c774e2289df54d1e7a
Alejo Orozco <orozcogomezalejandro@gmail.com>
2023-11-22 14:10:17 -0500

    - Problem with CREATE ADOPTIONDETAIL (CREATE) Solved

--- API_Adoptame/DAL/Entities/AdoptionDetail.cs
+++ API_Adoptame/DAL/Entities/AdoptionDetail.cs
@@ -9,13 +9,13 @@ namespace API_Adoptame.DAL.Entities
         public virtual Guid IDadoptiondetail { get; set; } //Sera la PK de todas las tablas de mi BD
 
         [Display(Name = "Fecha De Adopción")]
-        [MaxLength(50, ErrorMessage = "El campo {0} debe tener un máximo de {1} caracteres")]
+        
         public DateTime? AdoptionDate { get; set; }
 
 
 
         [Display(Name = "Fecha De Ingreso")]
-        [MaxLength(50, ErrorMessage = "El campo {0} debe tener un máximo de {1} caracteres")]
+        
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
         public DateTime AdmissionDate { get; set; }
 
